71b5a7fc13ee5d5d871c5a4edc129345821763f2
Miguel Peixe <miguel@cardume.art.br>
2016-10-18 17:28:41 -0200

    more design, start integrating modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,134 +1,146 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    browserify: {
-      all: {
-        files: {
-          'public/js/app.js': 'src/js/index.js'
-        }
-      }
-    },
-    uglify: {
-      all: {
-        options: {
-          mangle: true,
-          compress: true,
-          sourceMap: true
-        },
-        files: {
-          'public/js/dist.js': 'public/js/app.js',
-        }
-      }
-    },
-    less: {
-      all: {
-        options: {
-          compress: true,
-          sourceMap: true
-        },
-        files: {
-          'public/css/app.css': 'src/css/main.less'
-        }
-      }
-    },
-    pug: {
-      all: {
-        options: {
-          pretty: true,
-          doctype: 'html'
-        },
-        files: [{
-          expand: true,
-          cwd: 'src',
-          src: ['**/*.pug', '!views/includes/**/*'],
-          dest: 'public',
-          ext: '.html'
-        }]
-      }
-    },
-    copy: {
-      all: {
-        files: [
-          {
-            cwd: 'src',
-            src: ['**', '!js/**', '!**/*.less', '!**/*.pug', '!**/*.js'],
-            dest: 'public',
-            expand: true
-          },
-          {
-            cwd: 'src',
-            src: ['**'],
-            dest: 'public/src',
-            expand: true
-          }
-        ]
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      css: {
-        files: 'src/css/**/*.less',
-        tasks: ['less']
-      },
-      pug: {
-        files: 'src/**/*.pug',
-        tasks: ['pug']
-      },
-      scripts: {
-        files: 'src/js/**/*.js',
-        tasks: ['browserify']
-      },
-      copy: {
-        files: ['src/**', '!src/**/*.less', '!src/**/*.pug', '!src/**/*.js'],
-        tasks: ['copy']
-      }
-    },
-    'gh-pages': {
-      options: {
-        base: 'public'
-      },
-      src: ['**']
-    }
-  });
+	grunt.initConfig({
+		browserify: {
+			all: {
+				files: {
+					'public/js/app.js': 'src/js/index.js'
+				}
+			}
+		},
+		uglify: {
+			all: {
+				options: {
+					mangle: true,
+					compress: true
+				},
+				files: {
+					'public/js/app.js': 'public/js/app.js',
+				}
+			}
+		},
+		less: {
+			all: {
+				options: {
+					compress: true,
+					sourceMap: true
+				},
+				files: {
+					'public/css/app.css': 'src/css/main.less'
+				}
+			}
+		},
+		pug: {
+			all: {
+				options: {
+					pretty: true,
+					doctype: 'html'
+				},
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: ['**/*.pug', '!views/includes/**/*'],
+					dest: 'public',
+					ext: '.html'
+				}]
+			}
+		},
+		copy: {
+			all: {
+				files: [
+					{
+						cwd: 'src',
+						src: ['**', '!js/**', '!**/*.less', '!**/*.pug', '!**/*.js'],
+						dest: 'public',
+						expand: true
+					},
+					{
+						cwd: 'src',
+						src: ['**'],
+						dest: 'public/src',
+						expand: true
+					}
+				]
+			},
+			modules: {
+				files: [
+					{
+						cwd: 'node_modules',
+						src: [
+							'skeleton-css/**/*',
+							'highcharts/**/*'
+						],
+						dest: 'public/lib',
+						expand: true
+					}
+				]
+			}
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			css: {
+				files: 'src/css/**/*.less',
+				tasks: ['less']
+			},
+			pug: {
+				files: 'src/**/*.pug',
+				tasks: ['pug']
+			},
+			scripts: {
+				files: 'src/js/**/*.js',
+				tasks: ['browserify']
+			},
+			copy: {
+				files: ['src/**', '!src/**/*.less', '!src/**/*.pug', '!src/**/*.js'],
+				tasks: ['copy']
+			}
+		},
+		'gh-pages': {
+			options: {
+				base: 'public'
+			},
+			src: ['**']
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-pug');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-gh-pages');
 
-  grunt.registerTask(
-    'javascript',
-    'Compile scripts.',
-    ['browserify', 'uglify']
-  );
+	grunt.registerTask(
+		'javascript',
+		'Compile scripts.',
+		['browserify', 'uglify']
+	);
 
-  grunt.registerTask(
-    'views',
-    'Compile views.',
-    ['pug', 'less']
-  );
+	grunt.registerTask(
+		'views',
+		'Compile views.',
+		['pug', 'less']
+	);
 
-  grunt.registerTask(
-    'build',
-    'Compiles everything.',
-    ['copy', 'javascript', 'views']
-  );
+	grunt.registerTask(
+		'build',
+		'Compiles everything.',
+		['copy', 'javascript', 'views']
+	);
 
-  grunt.registerTask(
-    'deploy',
-    'Build and deploy to GitHub Pages.',
-    ['build', 'gh-pages']
-  )
+	grunt.registerTask(
+		'deploy',
+		'Build and deploy to GitHub Pages.',
+		['build', 'gh-pages']
+	)
 
-  grunt.registerTask(
-    'default',
-    'Build and start watching.',
-    ['build', 'watch']
-  );
+	grunt.registerTask(
+		'default',
+		'Build and start watching.',
+		['build', 'watch']
+	);
 
 }
---
+++ src/js/highcharts-defaults.js
@@ -0,0 +1,42 @@
+module.exports = {
+	options: {
+		chart: {
+			type: 'spline',
+			backgroundColor: null,
+			plotBackgroundColor: null,
+			style: {
+				fontFamily: 'Open Sans',
+				color: '#ffffff'
+			}
+		},
+		tooltip: {
+			style: {
+				padding: 10,
+				fontWeight: 'bold'
+			}
+		},
+		title: false,
+		subtitle: false,
+		legend: {
+			enabled: false
+		},
+		yAxis: {
+			labels: {
+				style: {
+					color: '#fff'
+				}
+			},
+			gridLineColor: 'rgba(255,255,255,0.2)'
+		},
+		xAxis: {
+			labels: {
+				style: {
+					color: '#fff'
+				}
+			}
+		},
+		credits: {
+			enabled: false
+		}
+	}
+};
