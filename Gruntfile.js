2e4d06c78b97ba7ce136091be75d49fc61e86fb4
miguelpeixe <miguel@cardume.art.br>
2016-10-18 11:24:33 -0200

    reorganize and add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,134 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    browserify: {
+      all: {
+        files: {
+          'public/js/app.js': 'src/js/index.js'
+        }
+      }
+    },
+    uglify: {
+      all: {
+        options: {
+          mangle: true,
+          compress: true,
+          sourceMap: true
+        },
+        files: {
+          'public/js/dist.js': 'public/js/app.js',
+        }
+      }
+    },
+    less: {
+      all: {
+        options: {
+          compress: true,
+          sourceMap: true
+        },
+        files: {
+          'public/css/app.css': 'src/css/main.less'
+        }
+      }
+    },
+    pug: {
+      all: {
+        options: {
+          pretty: true,
+          doctype: 'html'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['**/*.pug', '!views/includes/**/*'],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
+    copy: {
+      all: {
+        files: [
+          {
+            cwd: 'src',
+            src: ['**', '!js/**', '!**/*.less', '!**/*.pug', '!**/*.js'],
+            dest: 'public',
+            expand: true
+          },
+          {
+            cwd: 'src',
+            src: ['**'],
+            dest: 'public/src',
+            expand: true
+          }
+        ]
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: 'src/css/**/*.less',
+        tasks: ['less']
+      },
+      pug: {
+        files: 'src/**/*.pug',
+        tasks: ['pug']
+      },
+      scripts: {
+        files: 'src/js/**/*.js',
+        tasks: ['browserify']
+      },
+      copy: {
+        files: ['src/**', '!src/**/*.less', '!src/**/*.pug', '!src/**/*.js'],
+        tasks: ['copy']
+      },
+      'gh-pages': {
+        options: {
+          base: 'public'
+        },
+        src: ['**']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-pug');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask(
+    'javascript',
+    'Compile scripts.',
+    ['browserify', 'uglify']
+  );
+
+  grunt.registerTask(
+    'views',
+    'Compile views.',
+    ['pug', 'less']
+  );
+
+  grunt.registerTask(
+    'build',
+    'Compiles everything.',
+    ['copy', 'javascript', 'views']
+  );
+
+  grunt.registerTask(
+    'deploy',
+    'Deploy to GitHub Pages',
+    ['build', 'gh-pages']
+  )
+
+  grunt.registerTask(
+    'default',
+    'Build, start server and watch.',
+    ['build', 'watch']
+  );
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# InfoAmazonia Colombia
