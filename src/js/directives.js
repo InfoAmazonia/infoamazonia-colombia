d448c8118055f5f17fafc948073784430dc46354
Miguel Peixe <miguel@cardume.art.br>
2016-10-24 16:28:28 -0200

    update grids and more map directive development

--- src/js/directives.js
+++ src/js/directives.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
 					sql: '=',
 					query: '=',
 					columns: '=',
-					dataTable: '='
+					table: '='
 				},
 				link: function(scope, element, attrs) {
 
@@ -41,108 +41,47 @@ module.exports = function(app) {
 						if(typeof grid !== 'undefined') {
 							map.removeLayer(grid);
 						}
-						if(scope.username && scope.query && scope.sql) {
-							updateLayer();
+						if(scope.username && scope.query && scope.sql && scope.columns) {
+
+							scope.column = scope.columns[1];
+
+							getCartoDBQuantiles(scope.sql, scope.table, scope.column, function(bins) {
+
+								var cartocss = getCartoCSS(scope.column, bins);
+
+								addLayers(cartocss);
+
+							});
+
 						}
 					});
 
-					function updateLayer() {
+					function addLayers(cartocss) {
 						var layerData = {
 							user_name: scope.username,
 							sublayers: [{
 								sql: scope.query,
-								cartocss: '#layer { polygon-fill: #f00; }',
-								// interactivity: 'departamentos'
+								cartocss: cartocss,
+								interactivity: scope.columns
 							}]
 						};
 						cartodb.Tiles.getTiles(layerData, function(tilesUrl, err) {
 							if(tilesUrl == null) {
 								console.log("error: ", err.errors.join('\n'));
 							} else {
-								grid = new L.UtfGrid(tilesUrl.grids[0][0]);
 								layer = L.tileLayer(tilesUrl.tiles[0]);
-								map.addLayer(grid);
 								map.addLayer(layer);
 								scope.sql.getBounds(scope.query).done(function(bounds) {
 									map.fitBounds(bounds);
 								});
+								grid = new L.UtfGrid(tilesUrl.grids[0][0] + '&callback={cb}');
+								map.addLayer(grid);
 								grid.on('mouseover', function(e) {
 									console.log('hover', e.data);
-								})
+								});
 							}
 						});
 					}
-
-					function init() {
-
-						getCartoDBQuantiles(sql, attrs.table, attrs.column, function(quantiles) {
-
-							sql.getBounds(boundSelect).done(function(bounds) {
-
-								var cartocss = getCartoCSS(attrs.table, attrs.color, quantiles, attrs.city);
-
-								cartodb.createLayer(map, {
-									user_name: attrs.user,
-									type: 'cartodb',
-									sublayers: [{
-										sql: select,
-										cartocss: cartocss,
-										interactivity: attrs.interactivity || 'value'
-									}],
-									options: {
-										tooltip: true
-									}
-								})
-								.addTo(map)
-								.done(function(layer) {
-
-									fixMap(map, bounds);
-
-									var sublayer = layer.getSubLayer(0);
-
-									var update = _.debounce(function() {
-
-										//update quantiles
-										getCartoDBQuantiles(sql, attrs.table, attrs.column, function(qts) {
-
-											quantiles = qts;
-
-											// set new cartocss
-											sublayer.set({'cartocss': getCartoCSS(table, attrs.color, quantiles, attrs.city)});
-
-											// update query
-											var select = 'SELECT ' + attrs.column + ' as value, * FROM ' + table;
-											if(attrs.where) {
-												select += ' WHERE ' + attrs.where;
-											}
-											sublayer.set({'sql': select});
-
-										});
-
-									}, 100);
-
-									attrs.$observe('group', update);
-									attrs.$observe('column', update);
-									attrs.$observe('color', update);
-
-									sublayer.setInteraction(true);
-
-									layer.on('featureOver', function(event, latlng, pos, data, layerIndex) {
-										$rootScope.$broadcast('cartodbFeatureOver', _.extend({id: attrs.group}, data));
-									});
-
-									layer.on('featureOut', function(event) {
-										$rootScope.$broadcast('cartodbFeatureOver', {id: attrs.group});
-									});
-
-								});
-
-							});
-
-						});
-
-					}
-
 				}
 			}
 		}
@@ -150,85 +89,25 @@ module.exports = function(app) {
 
 };
 
-function getCartoCSS(table, color, quantiles, city) {
-
-	var hex = hexToRgb(color);
+function getCartoCSS(column, quantiles) {
 
 	var cartocss = [
-		'#' + table + ' { polygon-fill: transparent; polygon-opacity: 1; line-width: 1; line-opacity: 0.5; line-color: #000; }',
-		'#' + table + '[ value <= 0 ] { polygon-fill: transparent; }'
+		'#layer { polygon-fill: transparent; polygon-opacity: 1; line-width: 1; line-opacity: 0.5; line-color: #000; }',
+		'#layer[ ' + column + ' <= 0 ] { polygon-fill: transparent; }'
 	];
 
-	_.each(quantiles, function(qt, i) {
-		cartocss.push('#' + table + '[ value >= ' + qt + ' ] { polygon-fill: rgba(' + hex + ', ' + ((i+1)/10) + ');	}');
+	quantiles.forEach(function(qt, i) {
+		cartocss.push('#layer[ ' + column + ' >= ' + qt + ' ] { polygon-fill: rgba(0,0,0, ' + ((i+1)/5) + ');	}');
 	});
-
-	if(city) {
-		cartocss.push('#' + table + '[ co_ibge3 = "' + city + '" ] { line-width: 2; line-color: #fff; line-opacity: 1; }');
-	}
 
 	return cartocss.join(' ');
 
 }
 
 function getCartoDBQuantiles(sql, table, column, cb) {
-	sql.execute('SELECT CDB_HeadsTailsBins(array_agg(cast(' + column + ' as numeric)), 10) FROM ' + table).done(function(data) {
+	sql.execute('SELECT CDB_HeadsTailsBins(array_agg(cast(' + column + ' as numeric)), 7) FROM ' + table).done(function(data) {
 		var bins = data.rows[0].cdb_headstailsbins;
+		console.log(data);
 		cb(bins);
 	});
-}
-
-function numberInterval(options, callback, endCallback) {
-
-	var options = options || {};
-
-	options = _.extend({
-		countTo: 0,
-		value: 0,
-		duration: 1000
-	}, options);
-
-	var num, refreshInterval, steps, step, increment, timeoutId;
-
-	var calculate = function () {
-		refreshInterval = 30;
-		step = 0;
-		timeoutId = null;
-		options.countTo = parseFloat(options.countTo) || 0;
-		options.value = parseFloat(options.value) || 0;
-		options.duration = parseInt(options.duration) || 500;
-
-		steps = Math.ceil(options.duration / refreshInterval);
-		increment = ((options.countTo - options.value) / steps);
-		num = options.value;
-	}
-
-	var tick = function () {
-		timeoutId = setTimeout(function () {
-			num += increment;
-			step++;
-			if (step >= steps) {
-				clearTimeout(timeoutId);
-				num = options.countTo;
-				if(typeof endCallback == 'function')
-					endCallback(options.countTo);
-			} else {
-				if(typeof callback == 'function') {
-					callback(num, options.countTo);
-				}
-				tick();
-			}
-		}, refreshInterval);
-
-	}
-
-	return function (newVal, oldVal) {
-		options.countTo = newVal || options.countTo;
-		options.value = oldVal || 0;
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
-		calculate();
-		tick();
-	}
 }
--- src/js/index.js
+++ src/js/index.js
@@ -16,17 +16,19 @@ var app = angular.module('ia-colombia', [
 	'$timeout',
 	function($rootScope, $scope, $timeout) {
 
-		$scope.user = 'infoamazonia'
-		$scope.sql = new cartodb.SQL({user: $scope.user});
-		$scope.query = '';
+		$scope.user = 'infoamazonia';
 		$scope.dataTable = 'ideam_deforestacion_anual';
+		$scope.geomTable = 'subregiones_nomdepto_area_estudio_amz4326';
+		$scope.queryWhere = 'data.departamentos = geom.nom_depto';
+
+		$scope.sql = new cartodb.SQL({user: $scope.user});
 		$scope.dataQuery = 'SELECT * FROM ' + $scope.dataTable;
 
 		$scope.sql.execute($scope.dataQuery).done(function(data) {
 			console.log(data);
 			var fields = [];
 			for(var key in data.fields) {
-				if(key !== 'the_geom' && key !== 'the_geom_webmercator')
+				if(key !== 'the_geom' && key !== 'the_geom_webmercator' && key !== 'cartodb_id')
 					fields.push(key);
 			}
 
@@ -34,7 +36,7 @@ var app = angular.module('ia-colombia', [
 
 				$scope.columns = fields;
 
-				$scope.query = 'SELECT geom.the_geom, geom.the_geom_webmercator, data.' + $scope.columns.join(', data.') + ' FROM ideam_deforestacion_anual as data, subregiones_nomdepto_area_estudio_amz4326 as geom WHERE data.departamentos = nom_depto GROUP BY data.cartodb_id, geom.cartodb_id';
+				$scope.query = 'SELECT geom.cartodb_id, geom.the_geom, geom.the_geom_webmercator, data.' + $scope.columns.join(', data.') + ' FROM ' + $scope.dataTable + ' as data, ' + $scope.geomTable + ' as geom WHERE ' + $scope.queryWhere + ' GROUP BY data.cartodb_id, geom.cartodb_id';
 
 			});
 
